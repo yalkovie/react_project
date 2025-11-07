32b8ad5c5a524a61521eb2116cd21080017aac4e
Ilya <yalkovie@mail.ru>
2025-11-07 19:27:34 +0300

    Второй

--- shrek/src/App.jsx
+++ shrek/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 // import shrek from 'https://algoritmikaclub.github.io/frontend/React-2/dist/assets/shrek-Cr5Fh38_.png'
 import arg from './Arg/arg.js'
-import Header from './Header/Header'
-import Window from './Window/Window'
 import './App.css'
+import Header from './Header/Header.jsx'
+import Window from './Window/Window.jsx'
+import Button from './Button/Button.jsx'
+import Footer from './Footer/Footer.jsx'
 
 function App() {
 
   return (
     <>
     <Header />
-    {arg.map((btn, i) => <Window key = {i} pic = {btn.pic} text = {btn.text} />)}
+    <Button />
+    <Footer />
     </>
   )
 }
---
+++ shrek/src/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+function Footer () {
+    return (
+        <footer><span>Жил да был в сказочном государстве большой зеленый великан по имени Шрэк.</span></footer>
+    )
+}
+
+export default Footer
